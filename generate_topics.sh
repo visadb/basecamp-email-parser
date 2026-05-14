115e7a52ffbb5aae677dc1edc712be723e775688
Visa de Bruijn <visa.de.bruijn@reaktor.com>
2026-05-14 17:59:19 +0300

    Include topic count before deduplication to see how active topic has been

--- generate_topics.sh
+++ generate_topics.sh
@@ -4,4 +4,4 @@ for f in *.eml; do
   python3 parse.py < $f \
     | xmllint --html --xpath "//a[contains(@href, '3.basecamp.com') and @class='decorated']" -\
     | gsed 's/.*href="\([^"]*\)"[^>]*>\([^<]*\)<.*/\2 \1/'
-done | gsed -e 's/#.*//' -e 's/\\x[0-9a-f][0-9a-f]//g' -e 's/^ *//' | sort -u > topics
+done | gsed -e 's/#.*//' -e 's/\\x[0-9a-f][0-9a-f]//g' -e 's/^ *//' | sort | uniq -c > topics
